e2fdbf19e4b421668aefc1d788ce6620e720705e
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-11-18 18:35:13 +0100

    first commit

---
+++ src/main/java/com/matiej/springsec_acl/SpringSecAclApplication.java
@@ -0,0 +1,13 @@
+package com.matiej.springsec_acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecAclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiej/springsec_acl/SpringSecAclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiej.springsec_acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecAclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
