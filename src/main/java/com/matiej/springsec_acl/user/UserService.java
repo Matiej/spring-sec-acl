f0a231db36c621cc7f4922793e670b9115be1d9a
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-11-21 20:41:15 +0100

    implement findAll to not to inecjt repostiory into controller

--- src/main/java/com/matiej/springsec_acl/user/UserService.java
+++ src/main/java/com/matiej/springsec_acl/user/UserService.java
@@ -2,9 +2,13 @@ package com.matiej.springsec_acl.user;
 
 import com.matiej.springsec_acl.global.exceptions.EmailExistsException;
 
+import java.util.List;
+
 public interface UserService {
 
     UserEntity registerNewUser(UserEntity user) throws EmailExistsException;
 
     UserEntity findUserByEmail(String email);
+
+    List<UserEntity> findAll();
 }
--- src/main/java/com/matiej/springsec_acl/user/UserServiceImpl.java
+++ src/main/java/com/matiej/springsec_acl/user/UserServiceImpl.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
@@ -21,6 +23,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserEntity findUserByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Cant find user: " + email));
+    }
+
+    @Override
+    public List<UserEntity> findAll() {
+        return userRepository.findAll();
     }
 
     private boolean emailExist(final String email) {
